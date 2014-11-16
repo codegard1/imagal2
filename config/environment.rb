76862b71bfd22e17d54d220852f3fce474c9232f
codegard1 <c.odegard@gmail.com>
2014-11-16 14:37:16 -0500

    first commit, after rails install

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Imagal2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imagal2::Application.config.secret_key_base = '8b80b05da3b720c247d0781ff00a5a84c25ffa2ab7dbef1228ddec33e26923f50fb07d63d5c334115e1a1809071e7649336abb535c36183e960f0a2e0885643b'
